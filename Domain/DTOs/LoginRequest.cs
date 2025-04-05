013c9329ede24a6898413d33fbd342fc26710a72
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2025-04-05 23:57:24 +0300

    Implement DTO models

---
+++ Domain/DTOs/LoginRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Domain.DTOs
+{
+    [DataContract]
+    public class LoginRequest
+    {
+        [DataMember(Name = "userName")]
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [DataMember(Name = "password")]
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+}
---
+++ Domain/DTOs/RegisterRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Domain.DTOs
+{
+    [DataContract]
+    public class RegisterRequest
+    {
+        [DataMember(Name = "userName")]
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [DataMember(Name = "email")]
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [DataMember(Name = "password")]
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+}
